03178556cf22b8c213c6b688ec961a899b7e7ba0
Connor <joyceco1@mullenmail.com>
2015-01-22 22:52:14 -0700

    Initial Commit

--- FightingGame/src/fighterRunner.java
+++ FightingGame/src/fighterRunner.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class fighterRunner
 	{
 	public static void main(String[] args)
 		{
+		Scanner input = new Scanner(System.in);
 		Fighter [] fighter = new Fighter [6];
 		fighter[0] = new Sahira();
 		fighter[1] = new Lucaror();
@@ -9,16 +11,22 @@ public class fighterRunner
 		fighter[3] = new Golomech();
 		fighter[4] = new Shyra();
 		fighter[5] = new Auriel();
+
+	System.out.println("Which fighter would you like?");
+	System.out.println("1. Sahira");
+	System.out.println("2. Lucaror");
+	System.out.println("3. Bruce");
+	System.out.println("4. Golomech");
+	System.out.println("5. Shyra");
+	System.out.println("6. Auriel");
+	int choice = input.nextInt();	
+	fighter[choice - 1].identity();
+	fighter[choice - 1].fight();
+	fighter[choice - 1].counter();
+	fighter[choice - 1].ability();
+	fighter[choice - 1].myFinisher.finalStrike();
+	fighter[choice - 1].superChecker();
 		
-		for (int i = 0; i < fighter.length; i++)
-			{	
-			fighter[i].identity();
-			fighter[i].fight();
-			fighter[i].counter();
-			fighter[i].ability();
-			fighter[i].myFinisher.finalStrike();
-			fighter[i].superChecker();
-			System.out.println();
-			}
+			
 		}
 	}
